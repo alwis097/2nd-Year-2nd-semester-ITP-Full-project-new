5cd0e5451e47a0f1d7eba75fee839fda5c382513
voja <voja@gmail.com>
2020-07-17 10:13:27 +0200

    08

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-        hello vue
+    <div class="container">
+        <Header />
     </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+
 export default {
-    components: {},
+    components: {
+        Header,
+    },
 };
 </script>
