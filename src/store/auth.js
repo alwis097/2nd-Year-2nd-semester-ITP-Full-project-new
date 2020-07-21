d62bfd7355c4c481f3f205be02ceec0e69ec0f01
voja <voja@gmail.com>
2020-07-21 11:12:29 +0200

    42

---
+++ src/store/auth.js
@@ -0,0 +1,24 @@
+import Axios from "axios";
+
+const loginUrl = `http://localhost:3000/login`;
+
+export default {
+    namespaced: true,
+
+    state: {
+        authenticated: false,
+    },
+    mutations: {
+        setAuthenticateds(state) {
+            state.authenticated = true;
+        },
+    },
+    actions: {
+        async authenticate(context, order) {
+            let response = await Axios.post(loginUrl, credentials);
+            if (response.data.success == true) {
+                context.commit("setAuthenticateds");
+            }
+        },
+    },
+};
